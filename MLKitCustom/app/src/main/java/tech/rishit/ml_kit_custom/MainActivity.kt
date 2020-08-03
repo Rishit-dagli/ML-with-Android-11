c1d35d6b541b4e0b790dbdaacceacd3ac21639bf
Rishit Dagli <rishit.dagli@gmail.com>
2020-08-03 14:20:58 +0000

    Create blank MLKitCustom project

---
+++ MLKitCustom/app/src/main/java/tech/rishit/ml_kit_custom/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.rishit.ml_kit_custom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
